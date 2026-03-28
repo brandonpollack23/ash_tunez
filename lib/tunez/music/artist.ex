6abf6f0a30bdb895d500ffcde9809be359d5a7cb
Brandon Pollack <brandonpollack23@gmail.com>
2026-03-28 18:44:32 +0900

    create read update

---
+++ lib/tunez/music/artist.ex
@@ -0,0 +1,35 @@
+defmodule Tunez.Music.Artist do
+  use Ash.Resource, otp_app: :tunez, domain: Tunez.Music, data_layer: AshPostgres.DataLayer
+
+  postgres do
+    table "artists"
+    repo Tunez.Repo
+  end
+
+  actions do
+    create :create do
+      accept [:name, :biography]
+    end
+
+    read :read do
+      primary? true
+    end
+
+    update :update do
+      accept [:name, :biography]
+    end
+  end
+
+  attributes do
+    uuid_primary_key :id
+
+    attribute :name, :string do
+      allow_nil? false
+    end
+
+    attribute :biography, :string
+
+    create_timestamp :inserted_at
+    update_timestamp :updated_at
+  end
+end
