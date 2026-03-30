9a4f4e27b00705700057d4dcf7a4179d85f6092b
Brandon Pollack <brandonpollack23@gmail.com>
2026-03-31 00:36:33 +0900

    change action v1

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -17,7 +17,22 @@ defmodule Tunez.Music.Artist do
     end
 
     update :update do
+      require_atomic? false
       accept [:name, :biography]
+
+      change fn changeset, _context ->
+               new_name = Ash.Changeset.get_attribute(changeset, :name)
+               previous_name = Ash.Changeset.get_data(changeset, :name)
+               previous_names = Ash.Changeset.get_data(changeset, :previous_names)
+
+               names =
+                 [previous_name | previous_names]
+                 |> Enum.uniq()
+                 |> Enum.reject(&(&1 == new_name))
+
+               Ash.Changeset.change_attribute(changeset, :previous_names, names)
+             end,
+             where: [changing(:name)]
     end
 
     destroy :destroy
